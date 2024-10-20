dbfd65ba0412907a9bc4ab82d921c06bf9fa09d7
Way-curious <tuyentieple@gmail.com>
2024-10-20 16:51:01 +0700

    Implement Log4j for TestNG, ReportNG and Extent Report version 2

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,6 +1,9 @@
 package commons;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -9,12 +12,23 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
 import org.testng.Reporter;
+import org.testng.annotations.BeforeSuite;
 
+import java.io.File;
 import java.time.Duration;
 import java.util.Random;
 
 public class BaseTest {
+    protected final Logger log;
     private WebDriver driver;
+
+    public BaseTest() {
+        log = LogManager.getLogger(getClass());
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
 
     protected WebDriver getBrowserDriver(String browserName) {
         BrowserList browserList = BrowserList.valueOf(browserName.toUpperCase());
@@ -88,9 +102,10 @@ public class BaseTest {
         boolean status = true;
         try {
             Assert.assertTrue(condition);
+            log.info("-----------------------PASSED-----------------------");
         } catch (Throwable e) {
             status = false;
-
+            log.info("-----------------------FAILED-----------------------");
             VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
             Reporter.getCurrentTestResult().setThrowable(e);
         }
@@ -101,8 +116,10 @@ public class BaseTest {
         boolean status = true;
         try {
             Assert.assertFalse(condition);
+            log.info("-----------------------PASSED-----------------------");
         } catch (Throwable e) {
             status = false;
+            log.info("-----------------------FAILED-----------------------");
             VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
             Reporter.getCurrentTestResult().setThrowable(e);
         }
@@ -113,11 +130,38 @@ public class BaseTest {
         boolean status = true;
         try {
             Assert.assertEquals(actual, expected);
+            log.info("-----------------------PASSED-----------------------");
         } catch (Throwable e) {
             status = false;
+            log.info("-----------------------FAILED-----------------------");
             VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
             Reporter.getCurrentTestResult().setThrowable(e);
         }
         return status;
+    }
+
+    @BeforeSuite
+    public void deleteFileInReport() {
+        // Remove all file in ReportNG screenshot (image)
+        deleteAllFileInFolder("ReportNGScreenShots");
+
+
+    }
+
+    public void deleteAllFileInFolder(String folderName) {
+        try {
+            String pathFolderDownload = GlobalConstants.PROJECT_PATH + File.separator + folderName;
+            File file = new File(pathFolderDownload);
+            File[] listOfFiles = file.listFiles();
+            if (listOfFiles.length != 0) {
+                for (int i = 0; i < listOfFiles.length; i++) {
+                    if (listOfFiles[i].isFile() && !listOfFiles[i].getName().equals("environment.properties")) {
+                        new File(listOfFiles[i].toString()).delete();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
     }
 }
